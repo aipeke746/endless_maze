f2edc5e51e0b1b8c570fd1b08deae9999aa8c49c
nakatsu746 <jilptfes746@gmail.com>
2024-01-14 15:58:55 +0900

    setting: eslint, prettier, husky

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,41 @@
 import Phaser from 'phaser';
 
 class TestScene extends Phaser.Scene {
-  constructor() {
-    super('testScene');
-  }
-  preload() {
-    this.load.setBaseURL('https://labs.phaser.io');
+    constructor() {
+        super('testScene');
+    }
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-  }
+    preload(): void {
+        this.load.setBaseURL('https://labs.phaser.io');
 
-  create() {
-    this.add.image(400, 300, 'sky');
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        this.load.image('red', 'assets/particles/red.png');
+    }
 
-    const logo = this.physics.add.image(400, 100, 'logo');
+    create(): void {
+        this.add.image(400, 300, 'sky');
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-  }
+        const logo = this.physics.add.image(400, 100, 'logo');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+    }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  parent: 'app',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 },
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'app',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+        },
     },
-  },
-  scene: TestScene,
+    scene: TestScene,
 };
 
-new Phaser.Game(config);
+new Phaser.Game(config); // eslint-disable-line no-new
