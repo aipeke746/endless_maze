d41ebce15f5318e15d96f0e376bf0b0c6143fb31
nakatsu746 <jilptfes746@gmail.com>
2024-01-14 15:43:32 +0900

    gh-pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         assetsInlineLimit: 0,
+        outDir: 'docs',
     },
+    server: {
+        host: true,
+    }
 });
