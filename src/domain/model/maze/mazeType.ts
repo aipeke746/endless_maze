cc44c6fd9e5fb4489e31c613c3fc3bf1488d8f93
nakatsu746 <jilptfes746@gmail.com>
2024-03-02 09:22:42 +0900

    迷路の生成方法の種類を表すenumの代わりにオブジェクトリテラルを使用

--- src/domain/model/maze/mazeType.ts
+++ src/domain/model/maze/mazeType.ts
@@ -5,15 +5,15 @@ export const MAZE_TYPE = {
     /**
      * 棒倒し法
      */
-    StickKnockDown: "棒倒し法",
+    StickKnockDown: '棒倒し法',
     /**
      * 穴掘り法
      */
-    DiggingOut: "穴掘り法",
+    DiggingOut: '穴掘り法',
     /**
      * 壁のばし法
      */
-    WallStretching: "壁のばし法",
-}
+    WallStretching: '壁のばし法',
+};
 
-export type MazeType = (typeof MAZE_TYPE)[keyof typeof MAZE_TYPE]
+export type MazeType = (typeof MAZE_TYPE)[keyof typeof MAZE_TYPE];
